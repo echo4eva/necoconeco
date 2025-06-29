a5251eb4bef8e5c9e231737b89f000296b59b700
echo4eva <perilecho@gmail.com>
2025-06-28 19:17:50 -0700

    fix: windows scripts

--- scripts/release/install-startup.bat
+++ scripts/release/install-startup.bat
@@ -1,10 +1,18 @@
 @echo off
 set SCRIPT_PATH=%~dp0run.bat
-set SHORTCUT_NAME="necoconeco.lnk"
-set STARTUP_PATH="%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+set STARTUP_FOLDER=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
+set SHORTCUT_NAME=necoconeco.lnk
+
+:: Construct the full, quoted path to the shortcut link file
+set "FULL_SHORTCUT_PATH=%STARTUP_FOLDER%\%SHORTCUT_NAME%"
 
 echo Creating startup shortcut...
-powershell -Command "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut(%STARTUP_PATH%\%SHORTCUT_NAME%); $s.TargetPath = '%SCRIPT_PATH%'; $s.Save()"
+echo Target: %SCRIPT_PATH%
+echo Link: %FULL_SHORTCUT_PATH%
 
+:: Execute the PowerShell command, ensuring paths are correctly quoted
+powershell -Command "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut('"%FULL_SHORTCUT_PATH%"'); $s.TargetPath = '%SCRIPT_PATH%'; $s.Save()"
+
+echo.
 echo necoconeco has been set to run on startup.
 pause
